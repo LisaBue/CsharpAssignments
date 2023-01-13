0af8aa232a747274537e66fdceac6d64d74d16c5
Lisa Kotzebue <lisa@flores.nl>
2023-01-13 18:43:49 +0100

    new class added

---
+++ Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    internal class Book
+    {
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+        List<double> grades;
+    }
+}
