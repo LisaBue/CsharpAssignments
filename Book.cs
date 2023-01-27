a18e2914fe64c17e242f63cfe8411ce00d718018
Lisa Kotzebue <lisa@flores.nl>
2023-01-27 14:03:15 +0100

    completed the task

--- Book.cs
+++ Book.cs
@@ -8,10 +8,34 @@ namespace GradeBook
 {
     internal class Book
     {
+        public Book(string name) 
+        {
+            grades = new List<double>();
+            this.name = name; 
+        }
         public void AddGrade(double grade)
         {
             grades.Add(grade);
         }
-        List<double> grades;
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (var number in grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
+            Console.ReadLine();
+        }
+
+        private List<double> grades;
+        private string name;
     }
 }
