6d557e1c0993582124075c6df1566f2c9fbcf807
Lisa Kotzebue <lisa@flores.nl>
2023-02-24 17:38:35 +0100

    exercise completed

--- ch-1-exercise-1/Program.cs
+++ ch-1-exercise-1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+using Domain;
+    {
+    var you = new Person();
+
+    you.SayHello();
+    }
---
+++ ch-1-exercise-1/src/Domain/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+    
+    namespace Domain
+        {
+            public class Person
+            {
+                private string name = "Lisa";
+
+                public void SayHello()
+                {
+                    Console.WriteLine("Hello " + name);
+                }
+            }
+        }
+
